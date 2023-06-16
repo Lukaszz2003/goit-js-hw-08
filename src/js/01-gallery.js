2923217318c90024b8ec262f2a01e2d22122f0a2
Łukasz Paradowski <123119119+Lukaszz2003@users.noreply.github.com>
2023-06-16 13:57:59 +0200

    new

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,11 +1,12 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
 
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+
 // Change code below this line
 
-console.log(galleryItems);
 const gallery = document.querySelector('.gallery');
 const items = [];
 
