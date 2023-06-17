ba6f9a4d0063e88df40a886d72fa006089a39ea1
Łukasz Paradowski <123119119+Lukaszz2003@users.noreply.github.com>
2023-06-17 13:59:12 +0200

    new

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input[name="email"]');
+const message = document.querySelector('textarea[name="message"]');
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
+form.addEventListener(
+  'input',
+  throttle(e => {
+    const save = {
+      email: email.value,
+      message: message.value,
+    };
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(save));
+  }, 500)
+);
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  console.log({ email: email.value, message: message.value });
+  form.reset();
+  localStorage.removeItem(LOCAL_STORAGE_KEY);
+});
+
+const load = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+const storageData = load(LOCAL_STORAGE_KEY);
+if (storageData) {
+  email.value = storageData.email;
+  message.value = storageData.message;
+}
